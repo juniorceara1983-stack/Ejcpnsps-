d8ca5378b27b2052754e390ca5ef203878096bb0
juniorceara1983-stack <juniorceara1983@gmail.com>
2026-04-02 16:59:08 -0300

    Update sw-ejc.js

--- sw-ejc.js
+++ sw-ejc.js
@@ -5,16 +5,26 @@ const assets = [
   './ejcmaracanau.jpeg'
 ];
 
+// Instala os arquivos essenciais
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(assets);
     })
   );
+  self.skipWaiting();
 });
 
+// Ativa o service worker imediatamente
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Gerencia as requisições
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
